b678c82707ebbff9b6023b357e5e8142863b7b62
Greyjoy <liaoshi2001@gmail.com>
2015-05-05 22:43:41 -0400

    tag recommend加强版

--- final_server/tagRecommender.py
+++ final_server/tagRecommender.py
@@ -11,40 +11,72 @@ def lemmalist(word):
     return syn_set
 
 
-f = codecs.open("raw_tags", encoding='utf-8')
-lines = f.read()
-f.close()
-lines = lines.split("\n")
-tags = []
-for line in lines:
-	rawTag = line.split()[:-2]
-	tag = " ".join(rawTag)
-	tags.append(tag.lower())
+def getTagSet():
+	f = codecs.open("raw_tags", encoding='utf-8')
+	lines = f.read()
+	f.close()
+	lines = lines.split("\n")
+	tags = []
+	for line in lines:
+		if len(line.split()) == 0:
+			continue
+		rawTag = line.split()[:-2]
+		tagPopularity = line.split()[-2].encode('utf-8')
+		tagPopularity = int (tagPopularity.replace(',', ''))
+		tag = " ".join(rawTag)
+		tags.append([tag.lower(),tagPopularity])
+	return tags
 
 
-def YuanyiYangBigHandsomeGuy(text):
+def keywords(text):
+	rake = RakeKeywordExtractor()
+	keywords = rake.extract(text, incl_scores=True)
+	return keywords
+
+def coreTags(text):
+	coreTags = []
+
+	tags = getTagSet()
 	rake = RakeKeywordExtractor()
 	keywords = rake.extract(text, incl_scores=True)
 	tagsInArticles = set()
-	tagsRecommended = set()
 	for keyword in keywords:
-		tagsInArticles.add(keyword[0])
-		similarWords = lemmalist(keyword[0])
+		tagsInArticles.add(keyword)
+	for keyword,score in tagsInArticles:
+		for tag,tagPopularity in tags:
+			if (tag.lower() == keyword.lower()):
+				coreTags.append([tag, tagPopularity*score])
+	coreTags = sorted(coreTags, key=lambda x: x[1], reverse=True)
+	return coreTags
+
+
+def additionalTags(text):
+	additionalTags = []
+	tags = getTagSet()
+	rake = RakeKeywordExtractor()
+	keywords = rake.extract(text, incl_scores=True)
+	similarWordDict = {}
+	tagsInArticles = set()
+	for keyword,score in keywords:
+		tagsInArticles.add(keyword)
+		similarWords = lemmalist(keyword)
 		if len(similarWords) != 0:
 			for word in similarWords:
-				tagsRecommended.add(word)
+				if ((word in tagsInArticles) == False):
+					similarWordDict[word] = score
 
-	finalTagsRecommend = []
+	for keyword in similarWordDict.keys():
+		for tag,tagPopularity in tags:
+			if (tag.lower() == keyword.lower()):
+				additionalTags.append([tag, tagPopularity*similarWordDict[keyword]])
+	additionalTags = sorted(additionalTags, key=lambda x: x[1], reverse=True)
+	return additionalTags[:20]
 
-	for tag in tagsInArticles:
-		if tag.lower() in tags:
-			finalTagsRecommend.append(tag)
-
-	for tag in tagsRecommended:
-		if tag.lower() in tags:
-			finalTagsRecommend.append(tag)
-
-	return finalTagsRecommend
 
 #Demo
-# print YuanyiYangBigHandsomeGuy("Apple is a good product.")
+f = codecs.open("testFile", encoding='utf-8')
+content = f.read()
+f.close()
+print keywords(content)
+print coreTags(content)
+print additionalTags(content)
